0bdfbf969aac5f82f2ba582a4c3b3b4bfca94557
Agust18 <agustincamera16@hotmail.com>
2024-06-26 19:53:58 -0300

    miranda

---
+++ api/data/miranda.php
@@ -0,0 +1,185 @@
+<?php
+
+$puntos[] = [
+  "latitud" => "-34.64015880566487", // CON MUCHOS DECIMALES
+  "longitud" => "-60.461727903969454",
+  "fecha" => "2023-06-16", // YYYY-MM-DD
+  "horario" => "MAÑANA", // MAYUSCULA
+  "tipo" => "CHICO_CHICO", // CON MAYUSCULA Y GUION BAJO
+  "danios_materiales" => "DAÑOS_MENORES", // TODO MAYUSCULA
+  "danios_fisicos" => "LEVE", // MAYUSCULA
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/choque-fuga-y-un-herido/",
+];
+
+$puntos[] = [
+  "latitud" => "-34.644275818511304",
+  "longitud" => "-60.47493486937314",
+  "fecha" => "2023-06-16",
+  "horario" => "TARDE",
+  "tipo" => "GRANDE_CHICO",
+  "danios_materiales" => "DAÑOS_MENORES",
+  "danios_fisicos" => "LEVE",
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/un-padre-y-su-hijo-heridos-en-un-accidente/",
+];
+
+
+$puntos[] = [
+    "latitud" => "-34.6320616990372",
+    "longitud" => "-60.47221133280558",
+    "fecha" => "2023-06-17",
+    "horario" => "MADRUGADA",
+    "tipo" => "GRANDE_GRANDE",
+    "danios_materiales" => "DAÑOS_GRAVES",
+    "danios_fisicos" => "SIN_DAÑOS",
+    "muertos" => 0,
+    "url" => "https://chacabucoenred.com/chocaron-un-auto-y-lo-arrastraron-varios-metros/",
+  ];
+  
+  $puntos[] = [
+    "latitud" => "-34.637306190459086",
+    "longitud" => "-60.45400057513394",
+    "fecha" => "2023-06-17",
+    "horario" => "MADRUGADA",
+    "tipo" => "GRANDE_GRANDE",
+    "danios_materiales" => "DAÑOS_GRAVES",
+    "danios_fisicos" => "SIN_DAÑOS",
+    "muertos" => 0,
+    "url" => "https://chacabucoenred.com/violento-choque-en-el-acceso/",
+  ];
+
+  $puntos[] = [
+    "latitud" => "-34.63917769324707",
+    "longitud" => "-60.47139550396956",
+    "fecha" => "2023-06-17",
+    "horario" => "TARDE",
+    "tipo" => "GRANDE_GRANDE",
+    "danios_materiales" => "DAÑOS_MENORES",
+    "danios_fisicos" => "SIN_DAÑOS",
+    "muertos" => 0,
+    "url" => "https://chacabucoenred.com/choque-entre-autos-9/",
+  ];
+
+  $puntos[] = [
+    "latitud" => "-34.63879310261286",
+    "longitud" => "-60.460251176983384",
+    "fecha" => "2023-06-17",
+    "horario" => "TARDE",
+    "tipo" => "GRANDE_CHICO",
+    "danios_materiales" => "DAÑOS_MENORES",
+    "danios_fisicos" => "SIN_DAÑOS",
+    "muertos" => 0,
+    "url" => "https://chacabucoenred.com/accidente-entre-una-moto-y-un-auto-6/",
+  ];
+
+  $puntos[] = [
+    "latitud" => "34.65773320027918",
+    "longitud" => "-60.47734885587561",
+    "fecha" => "2023-06-17",
+    "horario" => "NOCHE",
+    "tipo" => "GRANDE_CHICO",
+    "danios_materiales" => "DAÑOS_MENORES",
+    "danios_fisicos" => "GRAVE",
+    "muertos" => 0,
+    "url" => "https://chacabucoenred.com/grave-accidente-de-transito-7/",
+  ];
+
+  $puntos[] = [
+    "latitud" => "-34.63588315616141",
+    "longitud" => "-60.488297978685",
+    "fecha" => "2023-06-20",
+    "horario" => "NOCHE",
+    "tipo" => "CHICO_CHICO",
+    "danios_materiales" => "DAÑOS_MENORES",
+    "danios_fisicos" => "LEVE",
+    "muertos" => 0,
+    "url" => "https://chacabucoenred.com/chocaron-dos-motos-una-mujer-embarazada-fue-trasladada-al-hospital/",
+  ];
+
+
+  $puntos[] = [
+    "latitud" => "-34.63049138406213",
+    "longitud" => "-60.465908988627355",
+    "fecha" => "2023-06-20",
+    "horario" => "TARDE",
+    "tipo" => "CHICO_CHICO",
+    "danios_materiales" => "DAÑOS_MENORES",
+    "danios_fisicos" => "LEVE",
+    "muertos" => 0,
+    "url" => "https://chacabucoenred.com/chocaron-dos-motos-una-mujer-embarazada-fue-trasladada-al-hospital/",
+  ];
+
+  $puntos[] = [
+    "latitud" => "-34.6328846005258",
+    "longitud" => " -60.49278770396986",
+    "fecha" => "2023-06-24",
+    "horario" => "TARDE",
+    "tipo" => "GRANDE_CHICO",
+    "danios_materiales" => "DAÑOS_MENORES",
+    "danios_fisicos" => "LEVE",
+    "muertos" => 0,
+    "url" => "https://chacabucoenred.com/menor-herido-en-accidente/",
+  ];
+
+  $puntos[] = [
+    "latitud" => "-34.63209929910529",
+    "longitud" => "-60.46554548862734",
+    "fecha" => "2023-06-24",
+    "horario" => "TARDE",
+    "tipo" => "GRANDE_CHICO",
+    "danios_materiales" => "DAÑOS_MENORES",
+    "danios_fisicos" => "LEVE",
+    "muertos" => 0,
+    "url" => "https://chacabucoenred.com/motociclista-trasladada-al-hospital/",
+  ];
+
+  $puntos[] = [
+    "latitud" => "-34.640330833034554",
+    "longitud" => "-60.474945446298285",
+    "fecha" => "2023-06-25",
+    "horario" => "MAÑANA",
+    "tipo" => "GRANDE_GRANDE",
+    "danios_materiales" => "DAÑOS_MENORES",
+    "danios_fisicos" => "SIN_DAÑOS",
+    "muertos" => 0,
+    "url" => "https://chacabucoenred.com/choque-entre-auto-y-camioneta-5/",
+  ];
+
+  $puntos[] = [
+    "latitud" => "-34.6481430235103",
+    "longitud" => "-60.45979534629766",
+    "fecha" => "2023-06-25",
+    "horario" => "TARDE",
+    "tipo" => "GRANDE_CHICO",
+    "danios_materiales" => "DAÑOS_MENORES",
+    "danios_fisicos" => "LEVE",
+    "muertos" => 0,
+    "url" => "https://chacabucoenred.com/tres-mujeres-heridas-en-un-accidente/",
+  ];
+
+  $puntos[] = [
+    "latitud" => "-34.65010583168172",
+    "longitud" => "-60.477411119311405",
+    "fecha" => "2023-06-28",
+    "horario" => "NOCHE",
+    "tipo" => "GRANDE_CHICO",
+    "danios_materiales" => "DAÑOS_MENORES",
+    "danios_fisicos" => "GRAVE",
+    "muertos" => 0,
+    "url" => "https://chacabucoenred.com/un-herido-en-un-accidente-entre-moto-y-auto/",
+  ];
+
+  $puntos[] = [
+    "latitud" => "-34.65594641692591",
+    "longitud" => "-60.466542319311166",
+    "fecha" => "2023-06-29",
+    "horario" => "MAÑANA",
+    "tipo" => "GRANDE_CHICO",
+    "danios_materiales" => "DAÑOS_MENORES",
+    "danios_fisicos" => "LEVE",
+    "muertos" => 0,
+    "url" => "https://chacabucoenred.com/un-joven-herido-tras-un-accidente-de-transito/",
+ ];
+
+?>
