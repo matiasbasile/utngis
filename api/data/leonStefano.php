d4e03b11c6d2bee267d57a1c28523ebfcc5ed921
leonstefano03 <leonstefano03@gmail.com>
2024-06-26 00:46:03 -0300

    leonStefano

---
+++ api/data/leonStefano.php
@@ -0,0 +1,181 @@
+<?php
+
+$puntos[] = [
+  "latitud" => "-34.6439373475418",
+  "longitud" => "-60.45705283942696",
+  "fecha" => "2023-01-31",
+  "horario" => "TARDE",
+  "tipo" => "GRANDE_CHICO",
+  "danios_materiales" => "DAÑOS_MENORES",
+  "danios_fisicos" => "GRAVE",
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/una-joven-sufrio-una-grave-herida-en-un-accidente/"
+];
+
+$puntos[] = [
+  "latitud" => "-34.63028109644353",
+  "longitud" => "-60.485913608716686",
+  "fecha" => "2023-01-31",
+  "horario" => "TARDE",
+  "tipo" => "GRANDE_CHICO",
+  "danios_materiales" => "DAÑOS_MENORES",
+  "danios_fisicos" => "LEVE",
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/mujer-herida-en-accidente-10/"
+];
+
+$puntos[] = [
+  "latitud" => "-34.63885339713595",
+  "longitud" => "-60.48287799336953",
+  "fecha" => "2023-01-30",
+  "horario" => "TARDE",
+  "tipo" => "GRANDE_GRANDE",
+  "danios_materiales" => "DAÑOS_MENORES",
+  "danios_fisicos" => "SIN_DAÑOS",
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/tres-autos-involucrados-en-un-accidente/"
+];
+
+$puntos[] = [
+  "latitud" => "-34.64298600812531",
+  "longitud" => "-60.469988748626164",
+  "fecha" => "2023-01-29",
+  "horario" => "NOCHE",
+  "tipo" => "CHICO_CHICO",
+  "danios_materiales" => "DAÑOS_MENORES",
+  "danios_fisicos" => "LEVE",
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/chocaron-dos-motos-en-pleno-centro/"
+];
+
+$puntos[] = [
+  "latitud" => "-34.63682615757405",
+  "longitud" => "-60.479735778028825",
+  "fecha" => "2023-01-27",
+  "horario" => "NOCHE",
+  "tipo" => "GRANDE_CHICO",
+  "danios_materiales" => "DAÑOS_MENORES",
+  "danios_fisicos" => "LEVE",
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/choque-entre-moto-y-auto-4/"
+];
+
+$puntos[] = [
+  "latitud" => "-34.64777617186883",
+  "longitud" => "-60.46567844733829",
+  "fecha" => "2023-01-27",
+  "horario" => "MAÑANA",
+  "tipo" => "GRANDE_GRANDE",
+  "danios_materiales" => "DAÑOS_GRAVES",
+  "danios_fisicos" => "GRAVE",
+  "muertos" => 1,
+  "url" => "https://chacabucoenred.com/gravisimo-accidente-de-transito-2/"
+];
+
+$puntos[] = [
+  "latitud" => "-34.6401911670571",
+  "longitud" => "-60.47028054734257",
+  "fecha" => "2023-01-25",
+  "horario" => "TARDE",
+  "tipo" => "GRANDE_CHICO",
+  "danios_materiales" => "SIN_DAÑOS",
+  "danios_fisicos" => "LEVE",
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/accidente-en-una-esquina-centrica/"
+];
+
+$puntos[] = [
+  "latitud" => "-34.63613755728482",
+  "longitud" => "-60.478893262687066",
+  "fecha" => "2023-01-23",
+  "horario" => "TARDE",
+  "tipo" => "GRANDE_CHICO",
+  "danios_materiales" => "DAÑOS_MENORES",
+  "danios_fisicos" => "GRAVE",
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/choque-entre-camioneta-y-moto-3/"
+];
+
+$puntos[] = [
+  "latitud" => "-34.633295778116754",
+  "longitud" => "-60.484292462688686",
+  "fecha" => "2023-01-23",
+  "horario" => "TARDE",
+  "tipo" => "GRANDE_CHICO",
+  "danios_materiales" => "DAÑOS_MENORES",
+  "danios_fisicos" => "LEVE",
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/joven-hospitalizada-tras-un-accidente-3/"
+];
+
+$puntos[] = [
+  "latitud" => "-34.64733034880272",
+  "longitud" => "-60.47615245023949",
+  "fecha" => "2023-01-23",
+  "horario" => "MAÑANA",
+  "tipo" => "GRANDE_GRANDE",
+  "danios_materiales" => "DAÑOS_GRAVES",
+  "danios_fisicos" => "LEVE",
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/choco-contra-una-planta/"
+];
+
+$puntos[] = [
+  "latitud" => "-34.63718806707429",
+  "longitud" => "-60.46251976268647",
+  "fecha" => "2023-01-22",
+  "horario" => "TARDE",
+  "tipo" => "GRANDE_CHICO",
+  "danios_materiales" => "DAÑOS_MENORES",
+  "danios_fisicos" => "LEVE",
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/una-menor-resulto-herida-en-un-accidente/"
+];
+
+$puntos[] = [
+  "latitud" => "-34.623931252202304",
+  "longitud" => "-60.48442481639951",
+  "fecha" => "2023-01-22",
+  "horario" => "TARDE",
+  "tipo" => "GRANDE_GRANDE",
+  "danios_materiales" => "DAÑOS_GRAVES",
+  "danios_fisicos" => "LEVE",
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/choco-contra-un-acoplado/"
+];
+
+$puntos[] = [
+  "latitud" => "-34.64814866701164",
+  "longitud" => "-60.46852007802244",
+  "fecha" => "2023-01-20",
+  "horario" => "NOCHE",
+  "tipo" => "GRANDE_CHICO",
+  "danios_materiales" => "DAÑOS_MENORES",
+  "danios_fisicos" => "LEVE",
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/un-herido-en-accidente-5/"
+];
+
+$puntos[] = [
+  "latitud" => "-34.6447764670309",
+  "longitud" => "-60.47107303199777",
+  "fecha" => "2023-01-17",
+  "horario" => "NOCHE",
+  "tipo" => "CHICO_CHICO",
+  "danios_materiales" => "DAÑOS_MENORES",
+  "danios_fisicos" => "LEVE",
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/un-repartidor-herido-en-un-accidente-de-transito/"
+];
+
+$puntos[] = [
+  "latitud" => "-34.647689952429126",
+  "longitud" => "-60.478842631996066",
+  "fecha" => "2023-01-16",
+  "horario" => "NOCHE",
+  "tipo" => "GRANDE_CHICO",
+  "danios_materiales" => "DAÑOS_MENORES",
+  "danios_fisicos" => "GRAVE",
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/choque-entre-auto-y-bici/"
+];
