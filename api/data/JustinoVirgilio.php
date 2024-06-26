7b5cb311d6ab5a62549f0ad7f8ca73712555518c
eljusti14 <justivirgilio@gmail.com>
2024-06-26 20:08:04 -0300

    justi

---
+++ api/data/JustinoVirgilio.php
@@ -0,0 +1,159 @@
+<?php 
+
+$puntos[] = [
+ "latitud" => "-34.64833312312769",
+ "longitud" => "-60.466466291922806",
+ "fecha" => "18-12-2022",
+ "horario" => "TARDE",
+ "tipo" => "GRANDE-CHICO",
+ "danios_materiales" => "DAÑOS_GRAVES",
+ "danios_fisicos" => "GRAVE",
+ "muertos" => 0,
+ "url" => "https://chacabucoenred.com/violento-accidente-6/",
+];
+
+$puntos[] = [
+ "latitud" => "-34.63974765810395",
+ "longitud" => "-60.46755159457423",
+ "fecha" => "21-12-2022",
+ "horario" => "TARDE",
+ "tipo" => "GRANDE-CHICO",
+ "danios_materiales" => "DAÑOS_LEVES",
+ "danios_fisicos" => "LEVE",
+ "muertos" => 0,
+ "url" => "https://chacabucoenred.com/accidente-un-joven-trasladado-al-hospital-5/",
+];
+
+$puntos[] = [
+ "latitud" => "-34.64364977919128",
+ "longitud" => "-60.47419423470296",
+ "fecha" => "23-12-2022",
+ "horario" => "MAÑANA",
+ "tipo" => "GRANDE-CHICO",
+ "danios_materiales" => "DAÑOS_LEVES",
+ "danios_fisicos" => "LEVE",
+ "muertos" => 0,
+ "url" => "https://chacabucoenred.com/accidente-entre-una-camioneta-y-una-moto-2/",
+];
+
+$puntos[] = [
+ "latitud" => "-34.65040011215553",
+ "longitud" => " -60.46222979385752",
+ "fecha" => "23-12-2022",
+ "horario" => "MAÑANA",
+ "tipo" => "GRANDE-CHICO",
+ "danios_materiales" => "DAÑOS_LEVES",
+ "danios_fisicos" => "SIN_DAÑOS",
+ "muertos" => 0,
+ "url" => "https://chacabucoenred.com/choque-entre-moto-y-camioneta-3/",
+];
+
+$puntos[] = [
+ "latitud" => "-34.6387575221046",
+ "longitud" => "-60.45554603733763",
+ "fecha" => "23-12-2022",
+ "horario" => "NOCHE",
+ "tipo" => "GRANDE-GRANDE",
+ "danios_materiales" => "DAÑOS_GRAVES",
+ "danios_fisicos" => "SIN_DAÑOS",
+ "muertos" => 0,
+ "url" => "https://chacabucoenred.com/violento-accidente-en-el-acceso-hipolito-yrigoyen/",
+];
+
+
+$puntos[] = [
+ "latitud" => "-34.63155604511614",
+ "longitud" => "-60.451716224190065",
+ "fecha" => "24-12-2022",
+ "horario" => "NOCHE",
+ "tipo" => "CHICO-CHICO",
+ "danios_materiales" => "DAÑOS_GRAVES",
+ "danios_fisicos" => "GRAVE",
+ "muertos" => 0,
+ "url" => "https://chacabucoenred.com/dos-heridos-en-un-choque-entre-motos-4/",
+];
+
+$puntos[] = [
+ "latitud" => "-34.64733511663571",
+ "longitud" => "-60.476160920321114",
+ "fecha" => "26-12-2022",
+ "horario" => "MADRUGADA",
+ "tipo" => "GRANDE-CHICO",
+ "danios_materiales" => "DAÑOS_GRAVES",
+ "danios_fisicos" => "GRAVE",
+ "muertos" => 0,
+ "url" => "https://chacabucoenred.com/joven-hospitalizada-tras-un-accidente-2/",
+];
+
+$puntos[] = [
+ "latitud" => "-34.631045007111744",
+ "longitud" => "-60.475295412026895",
+ "fecha" => "26-12-2022",
+ "horario" => "NOCHE",
+ "tipo" => "GRANDE-CHICO",
+ "danios_materiales" => "SIN_DAÑOS",
+ "danios_fisicos" => "LEVE",
+ "muertos" => 0,
+ "url" => "https://chacabucoenred.com/un-herido-en-accidente-4/",
+];
+
+$puntos[] = [
+ "latitud" => "-34.64057173995721",
+ "longitud" => "-60.483227790210265",
+ "fecha" => "27-12-2022",
+ "horario" => "TARDE",
+ "tipo" => "GRANDE-CHICO",
+ "danios_materiales" => "DAÑOS_GRAVES",
+ "danios_fisicos" => "GRAVE",
+ "muertos" => 0,
+ "url" => "https://chacabucoenred.com/joven-sufrio-una-grave-herida-en-un-accidente/",
+];
+
+$puntos[] = [
+ "latitud" => "-34.64924435871925",
+ "longitud" => "-60.45867339007475",
+ "fecha" => "27-12-2022",
+ "horario" => "TARDE",
+ "tipo" => "GRANDE-CHICO",
+ "danios_materiales" => "SIN_DAÑOS",
+ "danios_fisicos" => "LEVE",
+ "muertos" => 0,
+ "url" => "https://chacabucoenred.com/una-menor-trasladada-al-hospital-2/",
+];
+
+
+$puntos[] = [
+ "latitud" => "-34.64513882427849",
+ "longitud" => "-60.47395251709698",
+ "fecha" => "27-12-2022",
+ "horario" => "NOCHE",
+ "tipo" => "GRANDE-CHICO",
+ "danios_materiales" => "SIN_DAÑOS",
+ "danios_fisicos" => "LEVE",
+ "muertos" => 0,
+ "url" => "https://chacabucoenred.com/joven-hospitalizado-tras-un-accidente/",
+];
+
+$puntos[] = [
+ "latitud" => "-34.654121654665566",
+ "longitud" => "-60.4707826994931",
+ "fecha" => "30-12-2022",
+ "horario" => "MAÑANA",
+ "tipo" => "GRANDE-CHICO",
+ "danios_materiales" => "DAÑOS_LEVES",
+ "danios_fisicos" => "GRAVE",
+ "muertos" => 0,
+ "url" => "https://chacabucoenred.com/choque-y-fuga-4/",
+];
+
+$puntos[] = [
+ "latitud" => "-34.64924435871925",
+ "longitud" => "-60.45867339007475",
+ "fecha" => "30-12-2022",
+ "horario" => "TARDE",
+ "tipo" => "CHICO-CHICO",
+ "danios_materiales" => "DAÑOS_LEVES",
+ "danios_fisicos" => "LEVE",
+ "muertos" => 0,
+ "url" => "https://chacabucoenred.com/se-repite-un-choque-en-una-misma-esquina/",
+];
