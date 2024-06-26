db7cf680ed4658da5e8b08a893db6a346c0cc0c3
Agust18 <agustincamera16@hotmail.com>
2024-06-26 20:01:22 -0300

    tomaschilano

---
+++ api/data/tomaschilano.php
@@ -0,0 +1,157 @@
+<?php
+
+$puntos[] = [
+  "latitud" => "-34.639080257490676", // CON MUCHOS DECIMALES
+  "longitud" => "-60.46479410448709",
+  "fecha" => "2024-04-16", // YYYY-MM-DD
+  "horario" => "TARDE", // MAYUSCULA
+  "tipo" => "GRANDE_CHICO", // CON MAYUSCULA Y GUION BAJO
+  "danios_materiales" => "DAÑOS_MENORES", // TODO MAYUSCULA
+  "danios_fisicos" => "GRAVE", // MAYUSCULA
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/choque-en-una-esquina-con-semaforos-fuera-de-servicio/",
+];
+
+$puntos[] = [
+  "latitud" => "-34.633963040817555", // CON MUCHOS DECIMALES
+  "longitud" => "-60.46984180448734",
+  "fecha" => "2024-04-16", // YYYY-MM-DD
+  "horario" => "TARDE", // MAYUSCULA
+  "tipo" => "GRANDE_CHICO", // CON MAYUSCULA Y GUION BAJO
+  "danios_materiales" => "DAÑOS_MENORES", // TODO MAYUSCULA
+  "danios_fisicos" => "GRAVE", // MAYUSCULA
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/category/accidentes/page/4/",
+];
+
+$puntos[] = [
+  "latitud" => "-34.64436671506822", // CON MUCHOS DECIMALES
+  "longitud" => "-60.47309531983218",
+  "fecha" => "2024-04-17", // YYYY-MM-DD
+  "horario" => "TARDE", // MAYUSCULA
+  "tipo" => "GRANDE_CHICO", // CON MAYUSCULA Y GUION BAJO
+  "danios_materiales" => "DAÑOS_MENORES", // TODO MAYUSCULA
+  "danios_fisicos" => "SIN_DAÑOS", // MAYUSCULA
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/choque-y-fuga-6/",
+];
+
+$puntos[] = [
+  "latitud" => "-34.64066208764077", // CON MUCHOS DECIMALES
+  "longitud" => "-60.45788126215975",
+  "fecha" => "2024-04-18", // YYYY-MM-DD
+  "horario" => "NOCHE", // MAYUSCULA
+  "tipo" => "GRANDE_CHICO", // CON MAYUSCULA Y GUION BAJO
+  "danios_materiales" => "DAÑOS_MENORES", // TODO MAYUSCULA
+  "danios_fisicos" => "GRAVE", // MAYUSCULA
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/una-ciclista-trasladada-al-hospital-5/",
+];
+
+$puntos[] = [
+  "latitud" => "-34.64434729549139", // CON MUCHOS DECIMALES
+  "longitud" => "-60.453216883942325",
+  "fecha" => "2024-04-19", // YYYY-MM-DD
+  "horario" => "TARDE", // MAYUSCULA
+  "tipo" => "GRANDE_GRANDE", // CON MAYUSCULA Y GUION BAJO
+  "danios_materiales" => "DAÑOS_MENORES", // TODO MAYUSCULA
+  "danios_fisicos" => "SIN_DAÑOS", // MAYUSCULA
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/choque-en-junin-y-espora/",
+];
+
+$puntos[] = [
+  "latitud" => "-34.646177050938135", // CON MUCHOS DECIMALES
+  "longitud" => "-60.46406393332301",
+  "fecha" => "2024-04-23", // YYYY-MM-DD
+  "horario" => "MAÑANA", // MAYUSCULA
+  "tipo" => "CHICO_PEATON", // CON MAYUSCULA Y GUION BAJO
+  "danios_materiales" => "SIN_DAÑOS", // TODO MAYUSCULA
+  "danios_fisicos" => "GRAVE", // MAYUSCULA
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/vecina-hospitalizada-tras-un-accidente-4/",
+];
+
+$puntos[] = [
+  "latitud" => "-34.640281167893704", // CON MUCHOS DECIMALES
+  "longitud" => "-60.46836809099613",
+  "fecha" => "2024-04-24", // YYYY-MM-DD
+  "horario" => "MAÑANA", // MAYUSCULA
+  "tipo" => "GRANDE_CHICO", // CON MAYUSCULA Y GUION BAJO
+  "danios_materiales" => "DAÑOS_MENORES", // TODO MAYUSCULA
+  "danios_fisicos" => "GRAVE", // MAYUSCULA
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/motociclista-hospitalizada-tras-un-accidente/",
+];
+
+$puntos[] = [
+  "latitud" => "-34.645433842742186", // CON MUCHOS DECIMALES
+  "longitud" => "-60.450071053604056",
+  "fecha" => "2024-04-24", // YYYY-MM-DD
+  "horario" => "TARDE", // MAYUSCULA
+  "tipo" => "CHICO_GRANDE", // CON MAYUSCULA Y GUION BAJO
+  "danios_materiales" => "DAÑOS_MENORES", // TODO MAYUSCULA
+  "danios_fisicos" => "GRAVE", // MAYUSCULA
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/violento-accidente-de-transito-en-la-ciudad/",
+];
+
+$puntos[] = [
+  "latitud" => "-34.653772717241345", // CON MUCHOS DECIMALES
+  "longitud" => "-60.47270411797722",
+  "fecha" => "2024-04-25", // YYYY-MM-DD
+  "horario" => "TARDE", // MAYUSCULA
+  "tipo" => "CHICO_CHICO", // CON MAYUSCULA Y GUION BAJO
+  "danios_materiales" => "DAÑOS_MENORES", // TODO MAYUSCULA
+  "danios_fisicos" => "GRAVE", // MAYUSCULA
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/choque-entre-moto-y-bicicleta-2/",
+];
+
+$puntos[] = [
+  "latitud" => "-34.65116400776065", // CON MUCHOS DECIMALES
+  "longitud" => "-60.46758730448635",
+  "fecha" => "2024-04-26", // YYYY-MM-DD
+  "horario" => "TARDE", // MAYUSCULA
+  "tipo" => "GRANDE_CHICO", // CON MAYUSCULA Y GUION BAJO
+  "danios_materiales" => "DAÑOS_MENORES", // TODO MAYUSCULA
+  "danios_fisicos" => "LEVE", // MAYUSCULA
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/una-motociclista-accidentada/",
+];
+
+$puntos[] = [
+  "latitud" => "-34.639611858565615", // CON MUCHOS DECIMALES
+  "longitud" => "-60.474158662159766",
+  "fecha" => "2024-04-28", // YYYY-MM-DD
+  "horario" => "MADRUGADA", // MAYUSCULA
+  "tipo" => "GRANDE_CHICO", // CON MAYUSCULA Y GUION BAJO
+  "danios_materiales" => "DAÑOS_MENORES", // TODO MAYUSCULA
+  "danios_fisicos" => "GRAVE", // MAYUSCULA
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/motociclista-trasladado-al-hospital-5/",
+];
+
+$puntos[] = [
+  "latitud" => "-34.6441602804242", // CON MUCHOS DECIMALES
+  "longitud" => "-60.47799660448683",
+  "fecha" => "2024-04-29", // YYYY-MM-DD
+  "horario" => "TARDE", // MAYUSCULA
+  "tipo" => "GRANDE_CHICO", // CON MAYUSCULA Y GUION BAJO
+  "danios_materiales" => "DAÑOS_MENORES", // TODO MAYUSCULA
+  "danios_fisicos" => "GRAVE", // MAYUSCULA
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/145692-2/",
+];
+
+$puntos[] = [
+  "latitud" => "-34.647352436656185", // CON MUCHOS DECIMALES
+  "longitud" => "-60.460990262159264",
+  "fecha" => "2024-04-30", // YYYY-MM-DD
+  "horario" => "TARDE", // MAYUSCULA
+  "tipo" => "CHICO_CHICO", // CON MAYUSCULA Y GUION BAJO
+  "danios_materiales" => "DAÑOS_MENORES", // TODO MAYUSCULA
+  "danios_fisicos" => "GRAVE", // MAYUSCULA
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/mujer-herida-en-un-choque-entre-motos/",
+];
