e3c72248d211a26e662e816b824a7982dff127a7
Franco Licata <franco09licata@gmail.com>
2024-06-26 19:30:28 -0300

    Franco Licata

---
+++ api/data/francolicata.php
@@ -0,0 +1,195 @@
+<?php
+
+$puntos[] = [
+  "latitud" => "-34.63052220838942",
+  "longitud" => "-60.44623843106591",
+  "fecha" => "2023-09-01",
+  "horario" => "TARDE",
+  "tipo" => "CHICO_PEATON",
+  "danios_materiales" => "DAÑOS_MENORES",
+  "danios_fisicos" => "LEVE",
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/dos-heridos-en-un-accidente-en-el-acceso/",
+];
+ 
+$puntos[] = [
+  "latitud" => "-34.644136069847605",
+  "longitud" => "-60.459402531131204",
+  "fecha" => "2023-09-02",
+  "horario" => "NOCHE",
+  "tipo" => "CHICO_CHICO",
+  "danios_materiales" => "DAÑOS_MENORES",
+  "danios_fisicos" => "LEVE",
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/joven-accidentado/",
+];
+ 
+$puntos[] = [
+  "latitud" => "-34.65926433174792",
+  "longitud" => "-60.47229931677032",
+  "fecha" => "2023-09-03",
+  "horario" => "MAÑANA",
+  "tipo" => "CHICO_PEATON",
+  "danios_materiales" => "SIN_DAÑOS",
+  "danios_fisicos" => "SIN_DAÑOS",
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/cayo-a-la-cuneta-2/",
+];
+ 
+$puntos[] = [
+  "latitud" => "-34.637019637162226",
+  "longitud" => "-60.47768706971256",
+  "fecha" => "2023-09-04",
+  "horario" => "MAÑANA",
+  "tipo" => "GRANDE_CHICO",
+  "danios_materiales" => "DAÑOS_MENORES",
+  "danios_fisicos" => "LEVE",
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/menor-herido-en-accidente-2/",
+];
+ 
+$puntos[] = [
+  "latitud" => "-34.63927533150046",
+  "longitud" => "-60.45166291568594",
+  "fecha" => "2023-09-05",
+  "horario" => "TARDE",
+  "tipo" => "CHICO_CHICO",
+  "danios_materiales" => "SIN_DAÑOS",
+  "danios_fisicos" => "SIN_DAÑOS",
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/un-joven-cayo-de-su-moto/",
+];
+ 
+$puntos[] = [
+  "latitud" => "-34.6386611729348",
+  "longitud" => "-60.47735397524321",
+  "fecha" => "2023-09-06",
+  "horario" => "TARDE",
+  "tipo" => "GRANDE_CHICO",
+  "danios_materiales" => "DAÑOS_MENORES",
+  "danios_fisicos" => "LEVE",
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/menor-herido-en-accidente-2/",
+];
+ 
+$puntos[] = [
+  "latitud" => "-34.6386611729348",
+  "longitud" => "-60.477375432914315",
+  "fecha" => "2023-09-06",
+  "horario" => "TARDE",
+  "tipo" => "GRANDE_CHICO",
+  "danios_materiales" => "DAÑOS_MENORES",
+  "danios_fisicos" => "LEVE",
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/un-motociclista-trasladado-al-hospital-12/",
+];
+ 
+$puntos[] = [
+  "latitud" => "-34.64590017293212",
+  "longitud" => "-60.454830004078396",
+  "fecha" => "2023-09-09",
+  "horario" => "MADRUGADA",
+  "tipo" => "GRANDE_CHICO",
+  "danios_materiales" => "DAÑOS_MENORES",
+  "danios_fisicos" => "GRAVE",
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/dos-accidentes-en-simultaneo-4/",
+];
+ 
+$puntos[] = [
+  "latitud" => "-34.65747862328755",
+  "longitud" => "-60.468046304077745",
+  "fecha" => "2023-09-09",
+  "horario" => "MADRUGADA",
+  "tipo" => "GRANDE_CHICO",
+  "danios_materiales" => "DAÑOS_MENORES",
+  "danios_fisicos" => "LEVE",
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/dos-accidentes-en-simultaneo-4/",
+];
+ 
+$puntos[] = [
+  "latitud" => "-34.64071747567802",
+  "longitud" => "-60.47967312800631",
+  "fecha" => "2023-09-09",
+  "horario" => "TARDE",
+  "tipo" => "GRANDE_CHICO",
+  "danios_materiales" => "DAÑOS_MENORES",
+  "danios_fisicos" => "LEVE",
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/un-herido-en-accidente-transito/",
+];
+ 
+$puntos[] = [
+  "latitud" => "-34.64131821222108",
+  "longitud" => "-60.467184928961835",
+  "fecha" => "2023-09-10",
+  "horario" => "MADRUGADA",
+  "tipo" => "GRANDE_GRANDE",
+  "danios_materiales" => "DAÑOS_GRAVES",
+  "danios_fisicos" => "LEVE",
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/violento-accidente-de-transito-15/",
+];
+ 
+$puntos[] = [
+  "latitud" => "-34.64109808019003",
+  "longitud" => "-60.469135917756574",
+  "fecha" => "2023-09-11",
+  "horario" => "MAÑANA",
+  "tipo" => "GRANDE_GRANDE",
+  "danios_materiales" => "DAÑOS_GRAVES",
+  "danios_fisicos" => "LEVE",
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/cuatro-vehiculos-involucrados-en-un-accidente/",
+];
+ 
+$puntos[] = [
+  "latitud" => "-34.65114693157427",
+  "longitud" => "-60.48274950981844",
+  "fecha" => "2023-09-12",
+  "horario" => "MAÑANA",
+  "tipo" => "GRANDE_CHICO",
+  "danios_materiales" => "DAÑOS_MENORES",
+  "danios_fisicos" => "LEVE",
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/un-herido-en-accidente-6/",
+];
+ 
+$puntos[] = [
+  "latitud" => "-34.64747122184312",
+  "longitud" => "-60.46766332799708",
+  "fecha" => "2023-09-13",
+  "horario" => "MAÑANA",
+  "tipo" => "GRANDE_GRANDE",
+  "danios_materiales" => "DAÑOS_MENORES",
+  "danios_fisicos" => "SIN_DAÑOS",
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/choque-entre-auto-y-camioneta-6/",
+];
+ 
+$puntos[] = [
+  "latitud" => "-34.64997748878442",
+  "longitud" => "-60.46605767687864",
+  "fecha" => "2023-09-13",
+  "horario" => "TARDE",
+  "tipo" => "GRANDE_CHICO",
+  "danios_materiales" => "DAÑOS_MENORES",
+  "danios_fisicos" => "SIN_DAÑOS",
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/accidente-de-transito-168/",
+];
+ 
+$puntos[] = [
+  "latitud" => "-34.646909828140714",
+  "longitud" => "-60.48667216205816",
+  "fecha" => "2023-09-14",
+  "horario" => "NOCHE",
+  "tipo" => "GRANDE_CHICO",
+  "danios_materiales" => "DAÑOS_MENORES",
+  "danios_fisicos" => "SIN_DAÑOS",
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/le-chocaron-la-camioneta-y-se-dieron-a-la-fuga-2/",
+];
+
+?>
