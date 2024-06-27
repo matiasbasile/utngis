86c05528d9052f20c1e553f8b2faf4b280dd484a
Agust18 <agustincamera16@hotmail.com>
2024-06-26 21:11:52 -0300

    matias rinaldi

---
+++ api/data/matiasrinaldi.php
@@ -0,0 +1,241 @@
+<?php
+
+$puntos[] = [
+  "latitud" => "-34.63974631461048", // CON MUCHOS DECIMALES
+  "longitud" => "-60.47413969594886",
+  "fecha" => "2022-11-16", // YYYY-MM-DD
+  "horario" => "MAÑANA", // MAYUSCULA
+  "tipo" => "GRANDE_CHICO", // CON MAYUSCULA Y GUION BAJO
+  "danios_materiales" => "DAÑOS_MENORES", // TODO MAYUSCULA
+  "danios_fisicos" => "SIN_DAÑOS", // MAYUSCULA
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/accidente-entre-una-camioneta-y-una-moto/",
+];
+
+$puntos[] = [
+  "latitud" => "-34.63121938568795", // CON MUCHOS DECIMALES
+  "longitud" => "-60.47007870435901",
+  "fecha" => "2022-11-16", // YYYY-MM-DD
+  "horario" => "MAÑANA", // MAYUSCULA
+  "tipo" => "GRANDE_CHICO", // CON MAYUSCULA Y GUION BAJO
+  "danios_materiales" => "DAÑOS_MENORES", // TODO MAYUSCULA
+  "danios_fisicos" => "SIN_DAÑOS", // MAYUSCULA
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/cerro-a-una-mujer-y-su-hijo-que-iban-en-moto-y-se-dio-a-la-fuga/",
+];
+
+$puntos[] = [
+  "latitud" => "-34.633093085572355", // CON MUCHOS DECIMALES
+  "longitud" => "-60.48423879673387",
+  "fecha" => "2022-11-16", // YYYY-MM-DD
+  "horario" => "MAÑANA", // MAYUSCULA
+  "tipo" => "GRANDE_CHICO", // CON MAYUSCULA Y GUION BAJO
+  "danios_materiales" => "DAÑOS_MENORES", // TODO MAYUSCULA
+  "danios_fisicos" => "LEVE", // MAYUSCULA
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/tercer-accidente-del-dia-un-herido/",
+];
+
+$puntos[] = [
+  "latitud" => "-34.6332259179127", // CON MUCHOS DECIMALES
+  "longitud" => "-60.47573313319487",
+  "fecha" => "2022-11-16", // YYYY-MM-DD
+  "horario" => "TARDE", // MAYUSCULA
+  "tipo" => "CHICO_CHICO", // CON MAYUSCULA Y GUION BAJO
+  "danios_materiales" => "DAÑOS_MENORES", // TODO MAYUSCULA
+  "danios_fisicos" => "LEVES", // MAYUSCULA
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/cuarto-accidente-choque-entre-dos-motos-uno-se-dio-a-la-fuga/",
+];
+
+$puntos[] = [
+  "latitud" => "-34.63424096329585", // CON MUCHOS DECIMALES
+  "longitud" => "-60.46564423526161",
+  "fecha" => "2022-11-19", // YYYY-MM-DD
+  "horario" => "MAÑANA", // MAYUSCULA
+  "tipo" => "CHICO", // CON MAYUSCULA Y GUION BAJO
+  "danios_materiales" => "SIN_DAÑOS", // TODO MAYUSCULA
+  "danios_fisicos" => "LEVE", // MAYUSCULA
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/una-mujer-cayo-de-su-bicicleta-y-fue-trasladada-al-hospital/",
+];
+
+$puntos[] = [
+  "latitud" => "-34.64018522690826", // CON MUCHOS DECIMALES
+  "longitud" => "-60.46176011207574",
+  "fecha" => "2022-11-19", // YYYY-MM-DD
+  "horario" => "TARDE", // MAYUSCULA
+  "tipo" => "GRANDE_CHICO", // CON MAYUSCULA Y GUION BAJO
+  "danios_materiales" => "DAÑOS_MENORES", // TODO MAYUSCULA
+  "danios_fisicos" => "LEVE", // MAYUSCULA
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/violento-accidente-un-motociclista-trasladado-al-hospital/",
+];
+
+$puntos[] = [
+  "latitud" => "-34.65195754106746", // CON MUCHOS DECIMALES
+  "longitud" => "-60.47502204668581",
+  "fecha" => "2022-11-19", // YYYY-MM-DD
+  "horario" => "TARDE", // MAYUSCULA
+  "tipo" => "CHICO_CHICO", // CON MAYUSCULA Y GUION BAJO
+  "danios_materiales" => "DAÑOS_MENORES", // TODO MAYUSCULA
+  "danios_fisicos" => "LEVE", // MAYUSCULA
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/chocaron-dos-motos-dos-heridos/",
+];
+
+$puntos[] = [
+  "latitud" => "-34.64053881437092", // CON MUCHOS DECIMALES
+  "longitud" => "-60.47301346203052",
+  "fecha" => "2022-11-19", // YYYY-MM-DD
+  "horario" => "NOCHE", // MAYUSCULA
+  "tipo" => "CHICO_CHICO", // CON MAYUSCULA Y GUION BAJO
+  "danios_materiales" => "DAÑOS_MENORES", // TODO MAYUSCULA
+  "danios_fisicos" => "LEVE", // MAYUSCULA
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/tercer-accidente-del-dia-4/",
+];
+
+$puntos[] = [
+  "latitud" => "-34.6412931544366", // CON MUCHOS DECIMALES
+  "longitud" => "-60.45861274668639",
+  "fecha" => "2022-11-21", // YYYY-MM-DD
+  "horario" => "MAÑANA", // MAYUSCULA
+  "tipo" => "GRANDE_CHICO", // CON MAYUSCULA Y GUION BAJO
+  "danios_materiales" => "DAÑOS_MENORES", // TODO MAYUSCULA
+  "danios_fisicos" => "LEVE", // MAYUSCULA
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/choque-entre-auto-y-moto-17/",
+];
+
+$puntos[] = [
+  "latitud" => "-34.64122936183651", // CON MUCHOS DECIMALES
+  "longitud" => "-60.47382979086651",
+  "fecha" => "2022-11-22", // YYYY-MM-DD
+  "horario" => "TARDE", // MAYUSCULA
+  "tipo" => "GRANDE_GRANDE", // CON MAYUSCULA Y GUION BAJO
+  "danios_materiales" => "DAÑOS_MENORES", // TODO MAYUSCULA
+  "danios_fisicos" => "SIN_DAÑOS", // MAYUSCULA
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/choque-entre-dos-autos-11/",
+];
+
+$puntos[] = [
+  "latitud" => "-34.6394571228671", // CON MUCHOS DECIMALES
+  "longitud" => "-60.4609473466865",
+  "fecha" => "2022-11-23", // YYYY-MM-DD
+  "horario" => "TARDE", // MAYUSCULA
+  "tipo" => "GRANDE_CHICO", // CON MAYUSCULA Y GUION BAJO
+  "danios_materiales" => "DAÑOS_MENORES", // TODO MAYUSCULA
+  "danios_fisicos" => "LEVE", // MAYUSCULA
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/joven-herida-en-un-nuevo-accidente/",
+];
+
+$puntos[] = [
+  "latitud" => "-34.648839677226235", // CON MUCHOS DECIMALES
+  "longitud" => "-60.46058730435798",
+  "fecha" => "2022-11-23", // YYYY-MM-DD
+  "horario" => "TARDE", // MAYUSCULA
+  "tipo" => "GRANDE_CHICO", // CON MAYUSCULA Y GUION BAJO
+  "danios_materiales" => "DAÑOS_MENORES", // TODO MAYUSCULA
+  "danios_fisicos" => "LEVE", // MAYUSCULA
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/una-pareja-hospitalizada-tras-un-accidente/",
+];
+
+$puntos[] = [
+  "latitud" => "-34.63329116258332", // CON MUCHOS DECIMALES
+  "longitud" => "-60.472538317978334",
+  "fecha" => "2022-11-27", // YYYY-MM-DD
+  "horario" => "MADRUGADA", // MAYUSCULA
+  "tipo" => "GRANDE_CHICO", // CON MAYUSCULA Y GUION BAJO
+  "danios_materiales" => "DAÑOS_GRAVES", // TODO MAYUSCULA
+  "danios_fisicos" => "GRAVE", // MAYUSCULA
+  "muertos" => 1,
+  "url" => "https://chacabucoenred.com/una-joven-fallecida-en-un-accidente-de-transito/",
+];
+
+$puntos[] = [
+  "latitud" => "-34.63187813815635", // CON MUCHOS DECIMALES
+  "longitud" => "-60.447762094571196",
+  "fecha" => "2022-11-27", // YYYY-MM-DD
+  "horario" => "MAÑANA", // MAYUSCULA
+  "tipo" => "CHICO", // CON MAYUSCULA Y GUION BAJO
+  "danios_materiales" => "DAÑOS_MENORES", // TODO MAYUSCULA
+  "danios_fisicos" => "LEVE", // MAYUSCULA
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/motociclista-hospitalizado-2/",
+];
+
+$puntos[] = [
+  "latitud" => "-34.65278293652538", // CON MUCHOS DECIMALES
+  "longitud" => "-60.467252146685794",
+  "fecha" => "2022-11-27", // YYYY-MM-DD
+  "horario" => "TARDE", // MAYUSCULA
+  "tipo" => "GRANDE_GRANDE", // CON MAYUSCULA Y GUION BAJO
+  "danios_materiales" => "DAÑOS_MENORES", // TODO MAYUSCULA
+  "danios_fisicos" => "SIN_DAÑOS", // MAYUSCULA
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/choque-entre-dos-autos-12/",
+];
+
+$puntos[] = [
+  "latitud" => "-34.64000620532303", // CON MUCHOS DECIMALES
+  "longitud" => "-60.47025904668646",
+  "fecha" => "2022-11-28", // YYYY-MM-DD
+  "horario" => "TARDE", // MAYUSCULA
+  "tipo" => "GRANDE_CHICO", // CON MAYUSCULA Y GUION BAJO
+  "danios_materiales" => "DAÑOS_MENORES", // TODO MAYUSCULA
+  "danios_fisicos" => "SIN_DAÑOS", // MAYUSCULA
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/accidente-en-el-centro-2/",
+];
+
+$puntos[] = [
+  "latitud" => "-34.63478105911528", // CON MUCHOS DECIMALES
+  "longitud" => "-60.49058386203085",
+  "fecha" => "2022-11-28", // YYYY-MM-DD
+  "horario" => "TARDE", // MAYUSCULA
+  "tipo" => "GRANDE_CHICO", // CON MAYUSCULA Y GUION BAJO
+  "danios_materiales" => "DAÑOS_MENORES", // TODO MAYUSCULA
+  "danios_fisicos" => "LEVE", // MAYUSCULA
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/choque-en-el-acc-juan-xxiii/",
+];
+
+$puntos[] = [
+  "latitud" => "-34.63137502573646", // CON MUCHOS DECIMALES
+  "longitud" => "-60.471403817851005",
+  "fecha" => "2022-11-28", // YYYY-MM-DD
+  "horario" => "NOCHE", // MAYUSCULA
+  "tipo" => "GRANDE_CHICO", // CON MAYUSCULA Y GUION BAJO
+  "danios_materiales" => "DAÑOS_MENORES", // TODO MAYUSCULA
+  "danios_fisicos" => "LEVE", // MAYUSCULA
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/una-camioneta-choco-a-dos-bicicletas/",
+];
+
+$puntos[] = [
+  "latitud" => "-34.6410465422022", // CON MUCHOS DECIMALES
+  "longitud" => "-60.48238281785037",
+  "fecha" => "2022-11-29", // YYYY-MM-DD
+  "horario" => "TARDE", // MAYUSCULA
+  "tipo" => "GRANDE_CHICO", // CON MAYUSCULA Y GUION BAJO
+  "danios_materiales" => "DAÑOS_MENORES", // TODO MAYUSCULA
+  "danios_fisicos" => "LEVE", // MAYUSCULA
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/accidente-entre-un-auto-y-una-bicicleta/",
+];
+
+$puntos[] = [
+  "latitud" => "-34.640516750266286", // CON MUCHOS DECIMALES
+  "longitud" => "-60.47323007996476",
+  "fecha" => "2022-11-29", // YYYY-MM-DD
+  "horario" => "TARDE", // MAYUSCULA
+  "tipo" => "GRANDE_CHICO", // CON MAYUSCULA Y GUION BAJO
+  "danios_materiales" => "DAÑOS_MENORES", // TODO MAYUSCULA
+  "danios_fisicos" => "LEVE", // MAYUSCULA
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/un-joven-herido-en-accidente-de-transito-2-1/",
+];
