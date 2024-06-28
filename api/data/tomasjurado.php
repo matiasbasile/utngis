0d4b1d4c6574542636574f8148404ca204dcb95c
Matias Schettino <59831088+matiassctt@users.noreply.github.com>
2024-06-27 22:08:20 -0300

    Add files via upload

---
+++ api/data/tomasjurado.php
@@ -0,0 +1,88 @@
+<?php
+$puntos= [
+    "latitud" => "-34.640102578532854", // CON MUCHOS DECIMALES
+    "longitud" => "-60.48364469124009",
+    "fecha" => "2024-09-16", // YYYY-MM-DD
+    "horario" => "TARDE", // MAYUSCULA
+    "tipo" => "CAMION AUTO", // CON MAYUSCULA Y GUION BAJO
+    "danios_materiales" => "GRANDE", // TODO MAYUSCULA
+    "danios_fisicos" => "LEVE", // MAYUSCULA
+    "muertos" => 0,
+    "url" => https://chacabucoenred.com/choque-entre-un-camion-y-una-camioneta-2/
+  ];
+  $puntos= [
+    "latitud" => "-34.63624348088465", // CON MUCHOS DECIMALES
+    "longitud" => "60.476934847063646",
+    "fecha" => "2024-09-18", // YYYY-MM-DD
+    "horario" => "TARDE", // MAYUSCULA
+    "tipo" => "AUTO", // CON MAYUSCULA Y GUION BAJO
+    "danios_materiales" => "LEVE", // TODO MAYUSCULA
+    "danios_fisicos" => "LEVE", // MAYUSCULA
+    "muertos" => 0,
+    "url" => https://chacabucoenred.com/mujer-herida-en-un-accidente-3/
+  ];
+  $puntos= [
+    "latitud" => "-34.63624348088465", // CON MUCHOS DECIMALES
+    "longitud" => "60.476934847063646",
+    "fecha" => "2024-09-18", // YYYY-MM-DD
+    "horario" => "TARDE", // MAYUSCULA
+    "tipo" => "MOTO AUTO", // CON MAYUSCULA Y GUION BAJO
+    "danios_materiales" => "GRANDE_CHICO", // TODO MAYUSCULA
+    "danios_fisicos" => "LEVE", // MAYUSCULA
+    "muertos" => 0,
+    "url" => https://chacabucoenred.com/mujer-herida-en-un-accidente-3/
+  ];
+  $puntos= 
+    "latitud" => "-34.63624348088465", // CON MUCHOS DECIMALES
+    "longitud" => "60.476934847063646",
+    "fecha" => "2024-09-18", // YYYY-MM-DD
+    "horario" => "TARDE", // MAYUSCULA
+    "tipo" => "MOTO", // CON MAYUSCULA Y GUION BAJO
+    "danios_materiales" => "SIN_DAÑOS", // TODO MAYUSCULA
+    "danios_fisicos" => "LEVE", // MAYUSCULA
+    "muertos" => 0,
+    "url" => https://chacabucoenred.com/menor-hospitalizado-tras-un-accidente/
+  $puntos= [
+    "latitud" => "-34.63624348088465", // CON MUCHOS DECIMALES
+    "longitud" => "60.476934847063646",
+    "fecha" => "2024-09-19", // YYYY-MM-DD
+    "horario" => "TARDE", // MAYUSCULA
+    "tipo" => "CHICO_PEATON", // CON MAYUSCULA Y GUION BAJO
+    "danios_materiales" => "SIN_DAÑOS", // TODO MAYUSCULA
+    "danios_fisicos" => "LEVE", // MAYUSCULA
+    "muertos" => 0,
+    "url" => https://chacabucoenred.com/mujer-herida-en-un-accidente-3/
+  ];
+  $puntos= [
+    "latitud" => "-34.6486590812715", // CON MUCHOS DECIMALES
+    "longitud" => " -60.46270762192247",
+    "fecha" => "2024-09-20", // YYYY-MM-DD
+    "horario" => "TARDE", // MAYUSCULA
+    "tipo" => "MOTO_AUTO", // CON MAYUSCULA Y GUION BAJO
+    "danios_materiales" => "LEVE", // TODO MAYUSCULA
+    "danios_fisicos" => "LEVE", // MAYUSCULA
+    "muertos" => 0,
+    "url" => https://chacabucoenred.com/accidente-de-transito-169/
+  ];
+  $puntos= 
+    "latitud" => "-34.64461799510953", // CON MUCHOS DECIMALES
+    "longitud" => " -60.471158818140935",
+    "fecha" => "2024-09-22", // YYYY-MM-DD
+    "horario" => "TARDE", // MAYUSCULA
+    "tipo" => "CHICO_PEATON", // CON MAYUSCULA Y GUION BAJO
+    "danios_materiales" => "SIN_DAÑOS", // TODO MAYUSCULA
+    "danios_fisicos" => "LEVE", // MAYUSCULA
+    "muertos" => 0,
+    "url" => https://chacabucoenred.com/choque-en-una-esquina-centrica/
+    $puntos= 
+    "latitud" => "-34.644546652052355", // CON MUCHOS DECIMALES
+    "longitud" => " 60.464554863268276",
+    "fecha" => "2024-09-30", // YYYY-MM-DD
+    "horario" => "TARDE", // MAYUSCULA
+    "tipo" => "MOTO_AUTO", // CON MAYUSCULA Y GUION BAJO
+    "danios_materiales" => "GRAVES", // TODO MAYUSCULA
+    "danios_fisicos" => "LEVE", // MAYUSCULA
+    "muertos" => 0,
+    "url" => https://chacabucoenred.com/accidente-de-transito-170/
+
+?>
