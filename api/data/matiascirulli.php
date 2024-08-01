db2f82b83222b5796d19adceb545f87e813cef51
MatiasCirulli <lococirulli23@gmail.com>
2024-08-01 11:18:51 -0300

    Matias Cirulli

--- api/data/matiascirulli.php
+++ api/data/matiascirulli.php
@@ -25,15 +25,124 @@ $puntos[] = [
 ];
 
 $puntos[] = [
-    "latitud" => "-34.63345957384567",
-    "longitud" => "-60.473757389096065",
-    "fecha" => "2024-04-12",
-    "horario" => "MAÑANA",
-    "tipo" => "GRANDE_GRANDE",
-    "danios_materiales" => "DAÑOS_MENORES",
-    "danios_fisicos" => "GRAVE",
-    "muertos" => 0,
-    "url" => "https://chacabucoenred.com/vecino-hospitalizado-tras-un-choque/",
-  ];
+  "latitud" => "-34.63345957384567",
+  "longitud" => "-60.473757389096065",
+  "fecha" => "2024-04-12",
+  "horario" => "MAÑANA",
+  "tipo" => "GRANDE_GRANDE",
+  "danios_materiales" => "DAÑOS_MENORES",
+  "danios_fisicos" => "GRAVE",
+  "muertos" => 0,
+  "url" => "https://chacabucoenred.com/vecino-hospitalizado-tras-un-choque/",
+];
+
+$semaforos[] = [
+  "latitud" => "-34.64012870900164",
+  "longitud" => "-60.457073811128424",
+  "fotomulta" => "0",
+];
+
+$semaforos[] = [
+  "latitud" => "-34.6427788319883",
+  "longitud" => "-60.46021572063895",
+  "fotomulta" => "0",
+];
+
+$semaforos[] = [
+  "latitud" => "-34.64497460434035",
+  "longitud" => "-60.4625126404679",
+  "fotomulta" => "0",
+];
+
+$semaforos[] = [
+  "latitud" => "-34.64695900549583",
+  "longitud" => "-60.46494250478692",
+  "fotomulta" => "0",
+];
+
+$semaforos[] = [
+  "latitud" => "-34.648338541348956",
+  "longitud" => "-60.466528759290945",
+  "fotomulta" => "1",
+];
+
+$semaforos[] = [
+  "latitud" => "-34.651054104691525",
+  "longitud" => "-60.46962952000624",
+  "fotomulta" => "0",
+];
+
+$semaforos[] = [
+  "latitud" => "-34.65317836962902",
+  "longitud" => "-60.47194334137749",
+  "fotomulta" => "0",
+];
+
+$semaforos[] = [
+  "latitud" => "-34.647508118244865",
+  "longitud" => "-60.474247560062935",
+  "fotomulta" => "0",
+];
+
+$semaforos[] = [
+  "latitud" => "-34.645727502994546",
+  "longitud" => "-60.47654757270127",
+  "fotomulta" => "0",
+];
+
+$semaforos[] = [
+  "latitud" => "-34.63937233414749",
+  "longitud" => "-60.478053844049505",
+  "fotomulta" => "0",
+];
+
+$semaforos[] = [
+  "latitud" => "-34.63797729533978",
+  "longitud" => "-60.47643192980503",
+  "fotomulta" => "0",
+];
+
+$semaforos[] = [
+  "latitud" => "-34.63591619659341",
+  "longitud" => "-60.47410964638589",
+  "fotomulta" => "0",
+];
+
+$semaforos[] = [
+  "latitud" => "-34.63382308180866",
+  "longitud" => "-60.47188122642997",
+  "fotomulta" => "0",
+];
+
+$semaforos[] = [
+  "latitud" => "-34.63477547448293",
+  "longitud" => "-60.47066791613652",
+  "fotomulta" => "0",
+];
+
+$semaforos[] = [
+  "latitud" => "-34.636520283453486",
+  "longitud" => "-60.46825649889677",
+  "fotomulta" => "0",
+];
+
+$semaforos[] = [
+  "latitud" => "-34.639234081439106",
+  "longitud" => "-60.464793752975595",
+  "fotomulta" => "0",
+];
+
+$semaforos[] = [
+  "latitud" => "-34.642094395095576",
+  "longitud" => "-60.481169348887356",
+  "fotomulta" => "0",
+];
+
+$semaforos[] = [
+  "latitud" => "-34.64029161823347",
+  "longitud" => "-60.48354017656688",
+  "fotomulta" => "0",
+];
 
 ?>
+
